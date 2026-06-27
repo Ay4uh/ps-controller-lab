0900859180d0fd1e3ab6bcb74a21bcb129d7f943
user <user@users-MacBook-Air.local>
2026-06-27 12:29:54 -0400

    Configure live Firebase Web SDK credentials for ay5uh-community

--- shared/config.js
+++ shared/config.js
@@ -1,4 +1,13 @@
 // Firebase Project Configuration
 // If these are left blank, the app will ask you to enter configuration in the moderator panel
 // or fall back to mock browser-level localStorage databases.
-window.FIREBASE_CONFIG = null;
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBs-k0yUcU0o3cumTaZV8HXp7aCvxUbzlk",
+  authDomain: "ay5uh-community.firebaseapp.com",
+  projectId: "ay5uh-community",
+  storageBucket: "ay5uh-community.firebasestorage.app",
+  messagingSenderId: "82846963733",
+  appId: "1:82846963733:web:837cebaf0487630c5e561c",
+  measurementId: "G-66ZC8EBVT5"
+};
+
